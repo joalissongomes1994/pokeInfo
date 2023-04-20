06d8a8bbd05fc57ad5e776a9b86e4c40f7b2e525
Joalisson Gomes <joalissongomes.webdev@gmail.com>
2023-04-20 15:22:07 -0300

    feat: Initial structure of the project

---
+++ app/src/main/java/joalissongomes/dev/pokeinfo/MainActivity.kt
@@ -0,0 +1,11 @@
+package joalissongomes.dev.pokeinfo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
